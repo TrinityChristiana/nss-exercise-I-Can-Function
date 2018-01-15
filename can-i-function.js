b81da964a15758986350fb706de6bb83eb52fd51
Trinity Terry <trinityterry@Trinitys-MacBook-Air.local>
2018-01-14 22:05:08 -0600

    Final Edit

---
+++ can-i-function.js
@@ -0,0 +1,14 @@
+/* Trinity Terry
+Date Created:1/14/2018
+Date Edited: 1/14/2018 */
+// Write a function named printPersonalizedMessage.
+// The function should take two parameters: user's name and user's favorite color.
+// The function should print out a message to the user: "Hey, [user's name], I see you like [user's favorite color]."
+function printPersonalizedMessage(userName, userColor){
+   console.log(`Hey, ${userName}, I see you like ${userColor}.`)
+}
+
+// Call the function using your name and your favorite color.
+printPersonalizedMessage(`Gabrielle`, `black`);
+
+// Try calling the function with other names and other favorite colors and watch how it changes the output.
